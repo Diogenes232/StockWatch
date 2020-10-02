7a12b73b5662f8760e0277cb7bd3339ada887961
Daniel Abbassi <daniel.abbassi@codecentric.de>
2020-10-02 10:25:46 +0200

    back to the start (from full tutorial)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { WatchlistsComponent }      from './watchlists/watchlists.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { WatchlistDetailComponent }  from './watchlist-detail/watchlist-detail.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: WatchlistDetailComponent },
-  { path: 'watchlists', component: WatchlistsComponent }
-  
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/watchlist.ts
+++
@@ -1,6 +0,0 @@
-export class Watchlist {
-
-    id: number;
-    name: string;
-
-}
